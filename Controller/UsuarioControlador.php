be46ee5e51611b48b01b53f6c71246c491a6dfe9
ZEROX <115658302+Z-E-R-O-X@users.noreply.github.com>
2024-10-12 10:00:18 -0500

    Registro Funcional

--- Controller/UsuarioControlador.php
+++ Controller/UsuarioControlador.php
@@ -30,6 +30,7 @@ class UsuarioControlador extends UsuarioModelo
             echo json_encode($alerta);
             exit();
         }
+
         /* ------------ Verificando integridad de los datos -----------------*/
         if (MainModelo::verificar_datos("[A-Za-zÀ-ÿ\s]{1,50}", $nombre)) {
             $alerta = [
@@ -41,6 +42,7 @@ class UsuarioControlador extends UsuarioModelo
             echo json_encode($alerta);
             exit();
         }
+
         if (MainModelo::verificar_datos("[A-Za-zÀ-ÿ\s]{1,50}", $apellido)) {
             $alerta = [
                 "Alerta" => "simple",
@@ -51,6 +53,7 @@ class UsuarioControlador extends UsuarioModelo
             echo json_encode($alerta);
             exit();
         }
+
         if (MainModelo::verificar_datos("[0-9]{10,10}", $celular)) {
             $alerta = [
                 "Alerta" => "simple",
@@ -61,6 +64,7 @@ class UsuarioControlador extends UsuarioModelo
             echo json_encode($alerta);
             exit();
         }
+
         if (
             MainModelo::verificar_datos("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\W_]).{8,}", $clave1) ||
             MainModelo::verificar_datos("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\W_]).{8,}", $clave2)
@@ -74,5 +78,89 @@ class UsuarioControlador extends UsuarioModelo
             echo json_encode($alerta);
             exit();
         }
+
+        /* ------------ Comprobar claves -----------------*/
+        if ($clave1 != $clave2) {
+            $alerta = [
+                "Alerta" => "simple",
+                "Titulo" => "Ocurrio un error Inesperado",
+                "Texto" => "Las CONTRASEÑAS ingresadas no coinciden",
+                "Tipo" => "error"
+            ];
+            echo json_encode($alerta);
+            exit();
+        } else {
+            $clave = MainModelo::encryption($clave1);
+        }
+
+        /* ------------ Comprobar Correo y celular-----------------*/
+        if (filter_var($correo, FILTER_VALIDATE_EMAIL)) {
+            $check_correo = MainModelo::ejecutar_consulta_simple("SELECT correo FROM usuarios 
+            WHERE correo='$correo'");
+            if ($check_correo->rowCount() > 0) {
+                $alerta = [
+                    "Alerta" => "simple",
+                    "Titulo" => "Ocurrio un error Inesperado",
+                    "Texto" => "El CORREO ingresado ya se encuentra registrado en el sistema",
+                    "Tipo" => "error"
+                ];
+                echo json_encode($alerta);
+                exit();
+            }
+        } else {
+            $alerta = [
+                "Alerta" => "simple",
+                "Titulo" => "Ocurrio un error Inesperado",
+                "Texto" => "El CORREO no coincide con el formato solicitado",
+                "Tipo" => "error"
+            ];
+            echo json_encode($alerta);
+            exit();
+        }
+        $check_celular = MainModelo::ejecutar_consulta_simple("SELECT celular FROM usuarios 
+         WHERE celular='$celular'");
+        if ($check_celular->rowCount() > 0) {
+            $alerta = [
+                "Alerta" => "simple",
+                "Titulo" => "Ocurrio un error Inesperado",
+                "Texto" => "El CELULAR ingresado ya se encuentra registrado en el sistema",
+                "Tipo" => "error"
+            ];
+            echo json_encode($alerta);
+            exit();
+        }
+
+        /* ------------ Subir datos a la BD -----------------*/
+
+        $datos_usuario_reg = [
+            "NOM" => $nombre,
+            "APE" => $apellido,
+            "COR" => $correo,
+            "CEL" => $celular,
+            "CLA" => $clave,
+            "FOT" => "null",
+            "ROL" => "usuario"
+        ];
+        
+        $agregar_usuario = UsuarioModelo::agregar_usuario_modelo($datos_usuario_reg);
+
+        if ($agregar_usuario->rowCount() == 1) {
+            $alerta = [
+                "Alerta" => "limpiar",
+                "Titulo" => "Usuario registrado",
+                "Texto" => "Los datos del usuario han sido registrados con éxito",
+                "Tipo" => "success"
+            ];
+        } else {
+            $alerta = [
+                "Alerta" => "simple",
+                "Titulo" => "Ocurrió un error inesperado",
+                "Texto" => "No hemos podido registrar el usuario",
+                "Tipo" => "error"
+            ];
+        }
+        
+        echo json_encode($alerta);
+        
     }
 }
--- Model/UsuarioModelo.php
+++ Model/UsuarioModelo.php
@@ -6,14 +6,16 @@ class UsuarioModelo extends MainModelo
 {
     /* ------------ Modelo para agregar usuarios -----------------*/
     protected static function agregar_usuario_modelo($datos){
-        $sql = MainModelo:: conectar()->prepare("INSERT INTO usuarios(nombre,apellido,correo,celular,clave) 
-        VALUES(:NOM, :APE, :COR, :CEL, :CLA)");
+        $sql = MainModelo:: conectar()->prepare("INSERT INTO usuarios(nombre,apellido,correo,celular,clave,foto_perfil,rol) 
+        VALUES(:NOM, :APE, :COR, :CEL, :CLA, :FOT, :ROL)");
 
         $sql -> bindParam(":NOM",$datos['NOM']);
         $sql -> bindParam(":APE",$datos['APE']);
         $sql -> bindParam(":COR",$datos['COR']);
         $sql -> bindParam(":CEL",$datos['CEL']);
         $sql -> bindParam(":CLA",$datos['CLA']);
+        $sql -> bindParam(":FOT",$datos['FOT']);
+        $sql -> bindParam(":ROL",$datos['ROL']);
         $sql -> execute();
         return $sql;
     }
